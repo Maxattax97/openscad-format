5df845ceb4774f6aec1aabbbcba4d6707ae541d3
Max O'Cull <max.ocull@protonmail.com>
2019-03-12 17:30:24 -0400

    Lint

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+/* eslint-disable no-console */
 const fs = require('fs-extra');
 const getStdin = require('get-stdin');
 const globby = require('globby');
 const clangFormat = require('clang-format');
 const tmp = require('tmp-promise');
 const path = require('path');
-const { promisify } = require('util');
 const { argv } = require('yargs')
   .usage('Usage: $0 [options]')
   .example('$0 -i input.scad -o output.scad', 'Formats input.scad and saves it as output.scad')
@@ -24,21 +24,19 @@ const { argv } = require('yargs')
 
 tmp.setGracefulCleanup();
 
-
-// variables:      ^\s*([$\w]+)\s*=\s*(.+)\s*;$
-// include:        ^\s*(include|use)\s*<([\.\w\/]*)>\s*$
-// nested braces (PCRE):       (\{([^{}]|(?R))*\})
 async function convertIncludesToClang(str) {
+  // eslint-disable-next-line no-useless-escape
   const regex = /^\s*(include|use)\s*<([\.\w\/]*)>\s*$/gm;
   const backup = []; // {type: 'include' | 'use', path: 'cornucopia/../source.scad'}
-  let matches;
+  let matches = regex.exec(str);
 
-  while ((matches = regex.exec(str)) !== null) {
+  while (matches !== null) {
     if (matches.index === regex.lastIndex) {
-      regex.lastIndex++;
+      regex.lastIndex += 1;
     }
 
     let entry = {};
+    // eslint-disable-next-line no-loop-func
     matches.forEach((match, groupIndex) => {
       if (groupIndex === 0) {
         entry = {};
@@ -49,34 +47,38 @@ async function convertIncludesToClang(str) {
         backup.push(entry);
       }
     });
+
+    matches = regex.exec(str);
   }
 
-  let updated = str.replace(regex, '');
+  const updated = str.replace(regex, '');
 
-  let newIncludes = [];
+  const newIncludes = [];
   backup.forEach((item) => {
     // Use include since we're converting to Clang.
     newIncludes.push(`#include <${item.path}>\n`);
   });
   newIncludes.push(updated);
 
-  return { str: newIncludes.join(''), backup: backup };
+  return { str: newIncludes.join(''), backup };
 }
 
 async function convertIncludesToScad(str, backup) {
+  // eslint-disable-next-line no-useless-escape
   const regex = /^\s*#include\s*<([\.\w\/]*)>\s*$/gmi;
   let fixed = str;
-  let matches;
+  let matches = regex.exec(str);
 
-  while ((matches = regex.exec(str)) !== null) {
+  while (matches !== null) {
     if (matches.index === regex.lastIndex) {
-      regex.lastIndex++;
+      regex.lastIndex += 1;
     }
 
     let entry = {};
+    // eslint-disable-next-line no-loop-func
     matches.forEach((match, groupIndex) => {
       if (groupIndex === 0) {
-        entry = {full: match};
+        entry = { full: match };
       } else if (groupIndex === 1) {
         entry.path = match;
         backup.forEach((item) => {
@@ -86,6 +88,8 @@ async function convertIncludesToScad(str, backup) {
         });
       }
     });
+
+    matches = regex.exec(str);
   }
 
   return fixed;
@@ -94,32 +98,32 @@ async function convertIncludesToScad(str, backup) {
 async function format(str, tmpDir) {
   function getClangFormattedString(file) {
     return new Promise((resolve, reject) => {
-        const result = [];
-        clangFormat(file, 'utf-8', 'file', () => {
-          resolve(result.join());
-        })
-          .on('data', buffer => result.push(buffer.toString()))
-          .on('err', err => reject(err));
-      });
+      const result = [];
+      clangFormat(file, 'utf-8', 'file', () => {
+        resolve(result.join());
+      })
+        .on('data', buffer => result.push(buffer.toString()))
+        .on('err', err => reject(err));
+    });
   }
 
-  let clangConversionResult = await convertIncludesToClang(str);
+  const clangConversionResult = await convertIncludesToClang(str);
 
   const virtualFile = {
-    path: path.join(tmpDir.path, 'input.scad')
+    path: path.join(tmpDir.path, 'input.scad'),
   };
   await fs.writeFile(virtualFile.path, clangConversionResult.str);
 
   let result = await getClangFormattedString(virtualFile);
 
-  result = await convertIncludesToScad(result, clangConversionResult.backup)
+  result = await convertIncludesToScad(result, clangConversionResult.backup);
 
   try {
     await fs.remove(virtualFile.path);
   } catch (err) {
     console.error('Failed to remove temporary input file');
   }
-  
+
   return result;
 }
 
@@ -169,7 +173,7 @@ async function main(input, output) {
 
   const resultList = [];
 
-  tmpDir = await tmp.dir({ unsafeCleanup: true });
+  const tmpDir = await tmp.dir({ unsafeCleanup: true });
 
   try {
     await fs.copy('.clang-format', path.join(tmpDir.path, '.clang-format'));
